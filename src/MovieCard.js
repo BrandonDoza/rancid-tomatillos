53bb5e612ea36a12f2a2efea5cb71a45f16d6ff4
laurel Bonal <laurelbonal@gmail.com>
2024-05-11 16:28:10 -0600

    Add styling to individual movie cards Co-authored-by: brandonjamesdoza@gmail.com

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -4,7 +4,7 @@ export default function MovieCard({title, posterPath, rating, getMovieDetail, id
     return(
         <div className="movie-card" id={id} onClick={e => getMovieDetail(e.target.id)}>
             <h1 className="movie-title">{title}</h1>
-            <img src={posterPath} className="movie-poster"/>
+            <img src={posterPath} alt={title} className="movie-poster"/>
             <h2 className="rating">{rating}</h2>
         </div>
     )
