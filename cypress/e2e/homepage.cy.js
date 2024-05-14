15c30a5fe4e9c971149448958bf12800c965dee2
laurel Bonal <laurelbonal@gmail.com>
2024-05-13 20:17:12 -0600

    Add testing for homepage Co-authored-by: brandonjamesdoza@gmail.com

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,4 +1,4 @@
-describe('template spec', () => {
+describe('homepage', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000');
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
@@ -6,7 +6,17 @@ describe('template spec', () => {
       fixture: './mockdata.json'
     })
   });
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
+  it('should display movies', () => {
+    cy.contains('nav', 'Rancid Tomatillos')
+    .get('.movies-display').find('.movie-card').should('have.length', 10)
+    .get('.movie-card').first().contains('Black Adam')
+    .get('.movie-card').last().contains('Maneater')
+    // cy.intercept('GET', `https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270`, {
+    //   statusCode: 200,
+    //   fixture: './singledata.json'
+    // })
+    // .get('.movies-display > :nth-child(1)').click()
+    
   })
+  
 })
